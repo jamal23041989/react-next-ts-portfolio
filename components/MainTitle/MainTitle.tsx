576fd31458d5a6edb8b897e16c6ac6d83598c702
jk <jk@MacBook-Air-jk.local>
2023-01-06 19:42:36 +0300

    first

---
+++ components/MainTitle/MainTitle.tsx
@@ -0,0 +1,10 @@
+import styles from '../../styles/mainTitle.module.scss'
+
+const MainTitle = ({ text }: { text: string }) => (
+  <h2 className={styles.title}>
+    {text.toUpperCase()}
+    <span className={styles.title__border}></span>
+  </h2>
+)
+
+export default MainTitle
